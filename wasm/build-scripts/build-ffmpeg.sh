98a4579ac7df05bdc92537df0bc92e6655c87f1d
beaudry12087 <armelbeaudry@gmail.com>
2023-05-07 19:51:01 -0400

    Update build-ffmpeg.sh

--- wasm/build-scripts/build-ffmpeg.sh
+++ wasm/build-scripts/build-ffmpeg.sh
@@ -11,7 +11,7 @@ if [[ "$FFMPEG_ST" != "yes" ]]; then
     -s USE_PTHREADS=1                             # enable pthreads support
     -s PROXY_TO_PTHREAD=1                         # detach main() from browser/UI main thread
     -o wasm/packages/core/dist/ffmpeg-core.js
-		-s INITIAL_MEMORY=2147483648                  # 2GB
+		-s INITIAL_MEMORY=1073741824                  # 1GB
   )
 else
   mkdir -p wasm/packages/core-st/dist
